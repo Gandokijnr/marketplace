8ab97c20423fc8c6c156100a67fc233ba26b4ff1
Gandoki <abahgideon111@gmail.com>
2024-05-27 09:44:28 -0700

    little code revamp

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,26 +13,28 @@
 
   <main>
        <div class="main-slider">
-        <Carousel :value="products" :numVisible="3" :numScroll="1" :responsiveOptions="responsiveOptions" circular :autoplayInterval="3000">
-            <template #item="slotProps">
-                <div class="border-1 surface-border border-round m-2  p-3">
-                    <div class="mb-3">
-                        <div class="relative mx-auto">
-                            <img :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.data.image" :alt="slotProps.data.name" class="w-full border-round" />
-                            <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data.inventoryStatus)" class="absolute" style="left:5px; top: 5px"/>
-                        </div>
-                    </div>
-                    <div class="mb-3 font-medium">{{ slotProps.data.name }}</div>
-                    <div class="flex justify-content-between align-items-center">
-                        <div class="mt-0 font-semibold text-xl">${{ slotProps.data.price }}</div>
-                        <span>
-                            <Button icon="pi pi-heart" severity="secondary" outlined />
-                            <Button icon="pi pi-shopping-cart" class="ml-2"/>
-                        </span>
-                    </div>
-                </div>
-            </template>
-        </Carousel>
+        <div class="card">
+          <Carousel :value="products" :numVisible="3" :numScroll="1" :responsiveOptions="responsiveOptions" circular :autoplayInterval="3000">
+              <template #item="slotProps">
+                  <div class="border-1 surface-border border-round m-2  p-3">
+                      <div class="mb-3">
+                          <div class="relative mx-auto">
+                              <img :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.data.image" :alt="slotProps.data.name" class="w-full border-round" />
+                              <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data.inventoryStatus)" class="absolute" style="left:5px; top: 5px"/>
+                          </div>
+                      </div>
+                      <div class="mb-3 font-medium">{{ slotProps.data.name }}</div>
+                      <div class="flex justify-content-between align-items-center">
+                          <div class="mt-0 font-semibold text-xl">${{ slotProps.data.price }}</div>
+                          <span>
+                              <Button icon="pi pi-heart" severity="secondary" outlined />
+                              <Button icon="pi pi-shopping-cart" class="ml-2"/>
+                          </span>
+                      </div>
+                  </div>
+              </template>
+          </Carousel>
+        </div>
        </div>
        <div class="infinite-scroll w-full bg-gray-100 rounded overflow-auto mt-5">
         <div class="scroll-content flex justify-evenly">
