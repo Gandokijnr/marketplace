1f35a399994d4f5d30c899718f495e951d4b42c7
Gandoki <abahgideon111@gmail.com>
2024-06-17 05:15:06 -0700

    chunksize increase

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 2000 // Set this to a higher value as needed
+  },
   plugins: [
     vue(),
   ],
